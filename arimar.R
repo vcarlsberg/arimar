b5c8c1b773edd4e067bf92da33b7c4e3990c9d84
vcarlsberg <v.carlsberg@gmail.com>
2020-02-03 14:36:01 +0700

    - Implementation of ARIMA, Auto-ARIMA & hybridmodel on R

---
+++ arimar.R
@@ -0,0 +1,50 @@
+library(forecast)
+library(forecastHybrid)
+data_outflow_5000<-data.frame(y=c(16124,19768,19339,
+                                  14345,14138,11061,
+                                  13066,16467,20864,
+                                  8858,7390,7560,
+                                  
+                                  6488,15359,6701,
+                                  9082,4638,8352,
+                                  7760,7575,7535,
+                                  12050,13257,13580,
+                                  
+                                  10931,10558,7824,
+                                  7574,9982,10606, 
+                                  10927,10954,11480, 
+                                  10967,13434,12881,
+                                  
+                                  19290,9190,9295, 
+                                  11101,10983,13737, 
+                                  12246,12544,11972, 
+                                  14370,27582,30735, 
+                                  
+                                  136470,24154,23482, 
+                                  10311,38409,68227, 
+                                  17580,10169,15827, 
+                                  14535,13835,16903 
+                                  
+                                  ))
+
+
+
+
+myts <- ts(data_outflow_5000, start=c(1994, 1), end=c(1998, 12), frequency=12)
+
+components.ts = decompose(myts)
+plot(components.ts)
+
+arima1<-arima(myts, order = c(1,0,0))
+accuracy(arima1)
+forecast(arima1, 5)
+plot(forecast(arima1, 5))
+
+arima_auto <- auto.arima(myts)
+accuracy(arima_auto)
+forecast(arima_auto, 5)
+plot(forecast(arima_auto, 5))
+
+hm<-hybridModel(myts, models = "an")
+plot(forecast(hm))
+accuracy(hm)
