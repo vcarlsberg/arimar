279d66327b84f34ba841e53049bb1e1f83becfcb
vcarlsberg <v.carlsberg@gmail.com>
2020-02-19 20:02:59 +0700

    Implementing varma & VAR

--- VARMA.R
+++ VARMA.R
@@ -6,16 +6,25 @@ library(readxl)
 library(e1071)
 library(Metrics)
 library(MTS)
+library(vars)
 
 Dataset_Surabaya <- read_excel("C:/Users/asus/OneDrive - Institut Teknologi Sepuluh Nopember/Kuliah/Thesis/Dataset_Surabaya.xlsx")
-data_outflow<-data.frame(K50=Dataset_Surabaya[["K50000"]],
-                         K20=Dataset_Surabaya[["K20000"]],
-                         K10=Dataset_Surabaya[["K10000"]],
-                         K5=Dataset_Surabaya[["K5000"]],
-                         K2=Dataset_Surabaya[["K2000"]])
+data_outflow<-data.frame(K50K=Dataset_Surabaya[["K50000"]],
+                         K20K=Dataset_Surabaya[["K20000"]],
+                         K10K=Dataset_Surabaya[["K10000"]],
+                         K5K=Dataset_Surabaya[["K5000"]],
+                         K1K=Dataset_Surabaya[["K1000"]],
+                         K5=Dataset_Surabaya[["K500"]])
 myts <- ts(data_outflow,start=c(1994, 1), end=c(2017, 12), frequency=12)
 #myts <- ts(data_outflow_10000, frequency=12)
 #myts_2018<-ts(data_outflow_10000[["y"]][289:300],start=c(2018, 1), end=c(2018, 12), frequency=12)
 
 Eccm(myts,maxp=12,maxq=12)
-model_varma<-VARMA(myts, p = 8, q = 5)
+model_varma<-VARMA(myts, p = 6, q =0)
+
+model_var<-VAR(myts,p=9)
+summary(model_var)
+
+model_var_2<-VARselect(myts,lag.max=48)
+summary(model_var_2)
+model_var_2$criteria
